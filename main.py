b5044a7b6fa76a03e0a34a996c16b1d9536cb2d7
Carlos Moyano <carlosmoya@gmail.com>
2024-05-09 08:45:51 -0300

    Adds json convertion to pandas.

--- main.py
+++ main.py
@@ -1,41 +1,24 @@
 import logging
-import requests
 import pandas as pd
-from config import API_KEY
+import time
+import json
+
 logging.basicConfig(level=logging.INFO)
 
-def get_historic_price():
-	url = "https://coinranking1.p.rapidapi.com/coin/Qwsogvtv82FCd/history"
-	querystring = {
-		"referenceCurrencyUuid":"yhjMzLPhuIDl", #USD id
-		"timePeriod":"30d"
-		}
+with open('./historical_data/daily_price_new.json', 'r') as f:
+	newly_data = json.load(f)
+	f.close()
 
-	headers = {
-		"X-RapidAPI-Key": API_KEY,
-		"X-RapidAPI-Host": "coinranking1.p.rapidapi.com"
-	}
-	try:
-		response = requests.get(url, headers=headers, params=querystring)
-	except requests.exceptions.RequestException as e:
-		logging.error(e)
-		exit()
+newly_data =newly_data['data']['klines']
 
-	if response.status_code != 200:
-		logging.error(f"API returned status code {response.status_code}")
-		logging.info(f"Raw response: {response.json()}")
-		exit()
-	return response.json()
-
-
-def convert_to_pandas(raw_data):
-	df = pd.DataFrame(raw_data)
-	df['timestamp'] = pd.to_datetime(df['timestamp'], unit='s')
-	df['price'] = pd.to_numeric(df['price'])
-	return df
-
-raw_data = get_historic_price()
-df = convert_to_pandas(raw_data['data']['history'])
-candled_price = df.groupby(pd.Grouper(key='timestamp', freq='1D')).agg(['first', 'max', 'min', 'last'])
-candled_price = candled_price['price']
-
+data_for_pandas = [
+	{
+		'timestamp': time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(int(datapoint[0])/1000)),
+  		'open': float(datapoint[1]),
+		'close': float(datapoint[2]),
+		'high': float(datapoint[3]),
+		'low': float(datapoint[4]),
+		'volume': float(datapoint[5]),
+	} for datapoint in newly_data
+]
+df = pd.DataFrame(data_for_pandas)
